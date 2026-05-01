845b169330a694b43eb8af6787a1251dfae4410d
sudhansu <ersshekhar05@gmail.com>
2026-05-01 21:30:53 +0530

    first commit

---
+++ Objects_in_js/index.js
@@ -0,0 +1,50 @@
+// let obj = {
+//     name:"Mohit",
+//     age:20
+// }
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log("hello");
+
+// let tanvi = {
+//     Address:"Sonipat",
+//     Designation:"Software Engineer",
+//     "salary":"10000000000000",
+//     contact_no:"12345",
+//     marks:{
+//         Cs:100,
+//         Maths:90
+//     },
+//     "tanvi's hobbies":"travelling"
+
+// }
+// console.log(tanvi);
+// console.log(tanvi.salary);
+// console.log(tanvi["salary"]);
+// console.log(tanvi["tanvi's hobbies"]);
+
+
+// console.log(tanvi.marks.Maths);
+// console.log(typeof(tanvi.Address));
+
+
+let tanvi = {
+    Address:"Sonipat",
+    Designation:"Software Engineer",
+    "salary":"10000000000000",
+    contact_no:"12345",
+    marks:{
+        Cs:100,
+        Maths:90
+    },
+    "tanvi's hobbies":"travelling",
+}
+
+console.log(tanvi.Address);
+
+
+
+
+
+
+
---
+++ variables_in_js/index.js
@@ -0,0 +1,87 @@
+// Variables in jsx
+// 1. Var 2. Let 3. Const
+// Hoisting -- A varibale before declaration
+// 
+
+// console.log(a);
+// var a = 10; //Global scope
+// // console.log(a);
+
+
+// console.log(b);  //TDZ -- Temporal Dead zone
+// let b = 20; // Local Scope
+// console.log(b);
+
+// {
+//     let d = 30;
+// }
+// console.log(d);
+
+
+// {
+//     const e = 40
+// }
+// console.log(e);
+
+
+
+// {
+//     var d = 40;
+// }
+// console.log(d);
+
+
+
+// console.log(b);
+// let b = 20;
+
+
+// const c = 30;
+// console.log(c);
+
+// let a = 10;
+// let a = 20;
+// console.log(a);
+
+// variable re-decleration and variable re-initialization
+// let a = 10 // Declaration
+// let a = 20 // Re-declaration
+// // a = 30; // re-initialization
+// // let -- re-initialization(allowed)--
+// //  let -- re-decleration(Not allowed)
+// console.log(a);
+
+
+// var a = 10;
+// var a = 20; // Re-declaration
+// a = 30;
+// console.log(a);
+
+// In case of Var  Re-declaration and re-initialization both are allowed
+//  In case of let re-initialization is allowed and  Re-declaration - not allowed
+
+
+
+// const a = 10;
+// // const a = 20;
+// a = 50
+// console.log(a);
+// // In case of const re-initialization is not allowed and  Re-declaration - not allowed
+
+
+
+// static and dynamic type programming language
+
+// float a  = 10;
+// a = 10;
+
+
+// Datatypes in js
+//  There are two types of Dtype 
+//  Premitive and non Premitive Data type
+// premitive --pre- Defined datatype 
+// Non-Premitive -- Defined by user (Like obj, arr, functions)
+// Array , Object, function
+
+
+
