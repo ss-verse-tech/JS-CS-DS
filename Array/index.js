3d3e38f48526ebc7059f9214d186e592d2ac7355
sudhansu <ersshekhar05@gmail.com>
2026-05-08 20:03:01 +0530

    updated code

---
+++ Array/index.js
@@ -0,0 +1,88 @@
+let arr = [1,2,3, false, true, "Shubh"];
+// console.log(arr);
+
+// arr.push(4)
+// console.log(arr);
+
+// arr.pop()
+
+// arr.unshift(10)
+// arr.shift()
+
+// slice and splice
+
+// slice
+ //(index_value, end-1)
+// console.log(arr.slice(4,10000));
+// arr[2] = 2;
+
+// console.log(arr.splice(1,3));
+// console.log(arr);
+
+
+//  Conecept of Distructuring  
+
+// let a = [1,2,3,4,5];
+
+// let [a,b,c, d, e, f] = [1,2,3,4,5]
+// // console.log(a);
+// console.log(f);
+
+// concept of spread operator
+
+// let arr = [1,2,3,4,5];
+// let arr1 = [6,7,8,9,10];
+// // let arr3 = arr.concat(arr1)
+// // console.log(arr3);
+// // console.log(...arr);
+// console.log(...arr, ...arr1);
+
+// if else (Tarnary operator)
+
+// a = 20;
+// console.log(a%2===0?"win":"Loss"); // Tarnary operator
+
+
+
+//  Loop, For loop, map, filter, reduce
+// arr = [1,2,3,"Yash", true, false]
+// let newArr = arr.map((val)=>{
+
+// })
+//  function -- function expression, callback function, IIFE , Nomral Function, Higher Order function
+
+// Nomral function
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(2,3)
+
+
+// // Function Expression
+// let func = function (){
+//     console.log("Function Expression");
+// }
+// func();
+
+// Callback function -> A function 
+// A callback function is a function passed as an argument to another function, which is executed later.
+
+// function add(a,b, func){
+//     console.log(a+b);
+//     func(a,b)
+// }
+
+// function subtract(a,b){
+//     console.log("I am calling from  subtract function", a-b);
+// }
+
+// add(2,5, subtract)
+// subtract()
+// add(2,5,subtract)
+
+// let arr1 = [50,10000,...arr]
+// console.log(...arr1);
+
+(function (){
+    console.log("I am IIFE");
+})();
--- Objects_in_js/index.js
+++ Objects_in_js/index.js
@@ -6,28 +6,6 @@
 // console.log(obj.age);
 // console.log("hello");
 
-// let tanvi = {
-//     Address:"Sonipat",
-//     Designation:"Software Engineer",
-//     "salary":"10000000000000",
-//     contact_no:"12345",
-//     marks:{
-//         Cs:100,
-//         Maths:90
-//     },
-//     "tanvi's hobbies":"travelling"
-
-// }
-// console.log(tanvi);
-// console.log(tanvi.salary);
-// console.log(tanvi["salary"]);
-// console.log(tanvi["tanvi's hobbies"]);
-
-
-// console.log(tanvi.marks.Maths);
-// console.log(typeof(tanvi.Address));
-
-
 let tanvi = {
     Address:"Sonipat",
     Designation:"Software Engineer",
@@ -37,14 +15,31 @@ let tanvi = {
         Cs:100,
         Maths:90
     },
-    "tanvi's hobbies":"travelling",
+    "tanvi's hobbies":"travelling"
 }
-
+console.log(tanvi);
 console.log(tanvi.Address);
+console.log(tanvi["tanvi's hobbies"]);
 
 
+console.log(tanvi.salary);
+// console.log(tanvi["salary"]);
+// console.log(tanvi["tanvi's hobbies"]);
 
 
+// console.log(tanvi.marks.Maths);
+// console.log(typeof(tanvi.Address));
 
 
-
+// let tanvi = {
+//     Address:"Sonipat",
+//     Designation:"Software Engineer",
+//     "salary":"10000000000000",
+//     contact_no:"12345",
+//     marks:{
+//         Cs:100,
+//         Maths:90
+//     },
+//     "tanvi's hobbies":"travelling",
+// }
+// console.log(tanvi.Address);
